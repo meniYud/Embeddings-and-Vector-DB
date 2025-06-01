b240435f73581546158dadd35ec7f6be87e01f50
meniYud <menyud@gmail.com>
2025-06-01 14:36:27 +0300

    Document load and split into chunks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -6,13 +7,26 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain_core.documents import Document
 
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
-def main():
-    print("Hello from embeddings-and-vector-db!")
+def load_documents():
+    file_path = os.path.join(os.getcwd(), "mediumblog2.txt")
+    loader = TextLoader(file_path, encoding="utf-8")
+    documents = loader.load()
+    return documents
+
+def split_documents(docs: List[Document]):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    chunks = text_splitter.split_documents(docs)
+    print(chunks)
+    
+    
 
 if __name__ == "__main__":
-    main()
+    print("Hello from embeddings-and-vector-db!")
+    docs = load_documents()
+    split_documents(docs)
